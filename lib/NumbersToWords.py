716eec09e0bd98d660215024995397ec801de0da
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2014-07-10 15:05:36 +0100

    Two tests, passed

--- lib/NumbersToWords.py
+++ lib/NumbersToWords.py
@@ -1,2 +1,6 @@
 class NumbersToWords(object):
-	pass
+	def __init__(self):
+		self.ONES = "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty".split(" ")
+
+	def convert_segment(self, number):
+		return self.ONES[number-1].capitalize()
--- tests/NumbersToWords_tests.py
+++ tests/NumbersToWords_tests.py
@@ -1,2 +1,12 @@
 from nose.tools import *
 from lib import NumbersToWords
+
+def test_1_prints_out_One():
+	numbers_to_words = NumbersToWords.NumbersToWords()
+
+	assert_equal(numbers_to_words.convert_segment(1), "One");
+
+def test_5_prints_out_Five():
+	numbers_to_words = NumbersToWords.NumbersToWords()
+
+	assert_equal(numbers_to_words.convert_segment(5), "Five");
