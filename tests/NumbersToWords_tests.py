81c3c0fceaed2bfd2882f92f04d95f91061fe398
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2014-07-10 15:13:37 +0100

    Corrected tests

--- tests/NumbersToWords_tests.py
+++ tests/NumbersToWords_tests.py
@@ -1,12 +1,15 @@
-from nose.tools import *
+from unittest import TestCase
 from lib import NumbersToWords
 
-def test_1_prints_out_One():
-	numbers_to_words = NumbersToWords.NumbersToWords()
+class NumbersToWordsTest(TestCase):
 
-	assert_equal(numbers_to_words.convert_segment(1), "One");
+	def test_1_prints_out_One(self):
+		numbers_to_words = NumbersToWords.NumbersToWords()
+		self.assertEqual(numbers_to_words.convert_segment(1), "One");
 
-def test_5_prints_out_Five():
-	numbers_to_words = NumbersToWords.NumbersToWords()
+	def test_5_prints_out_Five(self):
+		numbers_to_words = NumbersToWords.NumbersToWords()
+		self.assertEqual(numbers_to_words.convert_segment(5), "Five");
 
-	assert_equal(numbers_to_words.convert_segment(5), "Five");
+	def test_12_prints_Twelve(self):
+		numbers_to_words = NumbersToWords.NumbersToWords()
